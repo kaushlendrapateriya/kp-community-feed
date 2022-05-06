49c35052ea27e028f773636b9ab37ffb45b665b4
Kaushlendra Pateriya <kaushlendrapateriya16@gmail.com>
2022-05-06 15:16:36 +0530

    Enabling Server Side Rendering(SSR) using express

---
+++ server/server.js
@@ -0,0 +1,37 @@
+import path from 'path';
+import fs from 'fs';
+import express from 'express';
+import React from 'react';
+import * as ReactDOMServer from 'react-dom/server';
+import { StaticRouter } from 'react-router-dom';
+import App from '../src/containers/App';
+
+const PORT = 8080;
+const app = express();
+
+app.use(express.static('./build'));
+
+app.get('/*', (req, res) => {
+    const context = {};
+    const app = ReactDOMServer.renderToString(
+        <StaticRouter location={req.url} context={context}>
+            <App />
+        </StaticRouter>,
+    );
+
+    const indexFile = path.resolve('./build/index.html');
+    fs.readFile(indexFile, 'utf8', (err, data) => {
+        if ( err ) {
+            console.error('Something went wrong: ', err);
+            return res.status(500).send('Oops, better luck next time!');
+        }
+
+        data = data.replace('<div id="root"></div>', `<div id="root">${app}</div>`);
+
+        return res.send(data);
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server-Side Rendered appliction running on port ${PORT}`);
+})
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import styled, { createGlobalStyle } from 'styled-components';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Header from '../components/Header/Header';
 import Feed from './Feed';
 import Question from './Question';
@@ -26,21 +26,19 @@ class App extends Component {
         <GlobalStyle />
         <AppWrapper>
           <Header />
-          <Router>
-            <Routes>
-              <Route path='/' element={<Feed />} />
-              <Route path='/questions/:qId' element={<Question />} />
-              <Route path='/questions/*' element={<Feed />} />              
-              <Route 
-                path='*' 
-                element={
-                  <main style={{ padding: "1rem" }}>
-                    <p>There is nothing here !</p>
-                  </main>
-                }
-              />
-            </Routes>            
-          </Router>
+          <Routes>
+            <Route path='/' element={<Feed />} />
+            <Route path='/questions/:qId' element={<Question />} />
+            <Route path='/questions/*' element={<Feed />} />              
+            <Route 
+              path='*' 
+              element={
+                <main style={{ padding: "1rem" }}>
+                  <p>There is nothing here !</p>
+                </main>
+              }
+            />
+          </Routes>            
         </AppWrapper>
       </>
     );
